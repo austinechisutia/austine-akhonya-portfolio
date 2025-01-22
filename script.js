eb5f0dcb2b5fc7c2374770f294046bd1c92d18b3
austinechisutia <austineakhonya624@gmail.com>
2025-01-22 23:03:41 +0300

    changed link icons

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ ScrollReveal().reveal('.home-content p, .about-content',  { origin: 'right' });
 
 // typed js
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'YouTuber', 'Blogger' ],
+    strings: ['Frontend Developer', 'Graphic Designer', 'Videography' ],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
