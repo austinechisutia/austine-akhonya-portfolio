b503054541b9f227db2284233c65cd4346f15f08
austinechisutia <austineakhonya624@gmail.com>
2025-02-03 22:08:16 +0300

    coreected some projects and edited some text

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ ScrollReveal().reveal('.home-content p, .about-content',  { origin: 'right' });
 
 // typed js
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Graphic Designer', 'Videography' ],
+    strings: ['Frontend Developer', 'Graphic Designer', 'Videographer' ],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
